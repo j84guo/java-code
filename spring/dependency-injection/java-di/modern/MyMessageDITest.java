90ade2b54f4b841282eea5c503f5fa708bc4f528
Jackson Guo <jackson.guo@nuance.com>
2017-11-26 18:14:07 -0500

    Java dependency injection

---
+++ spring/dependency-injection/java-di/modern/MyMessageDITest.java
@@ -0,0 +1,24 @@
+public class MyMessageDITest {
+
+	public static void main(String[] args) {
+		String msg = "Hi Pankaj";
+		String email = "pankaj@abc.com";
+		String phone = "4088888888";
+		MessageServiceInjector injector = null;
+		Consumer app = null;
+		
+		// injector creates application and injects with dependency
+		// notice application classes are only resonsible got using the service
+		
+		//Send email
+		injector = new EmailServiceInjector(); 
+		app = injector.getConsumer(); 
+		app.processMessages(msg, email);
+		
+		//Send SMS
+		injector = new SMSServiceInjector();
+		app = injector.getConsumer();
+		app.processMessages(msg, phone);
+	}
+
+}
