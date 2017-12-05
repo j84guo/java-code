3fb5b8c49d5b065c5fe91379b7f5837b902400d4
Jackson Guo <guo.jackson@outlook.com>
2017-12-05 13:39:00 -0500

    spring rest service

---
+++ spring/spring-boot/rest-template/src/main/java/rest/RestApplication.java
@@ -0,0 +1,34 @@
+package rest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class RestApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(RestApplication.class);
+
+	public static void main(String args[]) {
+		SpringApplication.run(RestApplication.class);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			Quote quote = restTemplate.getForObject(
+					"http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+			log.info(quote.toString());
+		};
+	}
+}
---
+++ spring/spring-boot/social-media-broker/src/main/java/social/SocialController.java
@@ -0,0 +1,21 @@
+package social;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class SocialController {
+
+    @RequestMapping("/")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
+    }
+
+    @RequestMapping("/springquote")
+    public String test() {
+        return "this is a test response";
+    }
+}
